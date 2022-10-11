fc117c2bdc19219a20a74fe33bfc5d57cc2de0e0
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-10-11 00:33:06 -0300

    Add toggle function to open Menu

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-
-let closeBtn = document.querySelector('.close-button');
-
-closeBtn.addEventListener('click', function(){
-  alert('Hello');
-  let mobileContainer = document.querySelector('.menu-mobile-container');
-  mobileContainer.classList.toggle("sm-none");
-  alert('Hello');
+const closeBtn = document.querySelector('.close-button');
+const mobileContainer = document.querySelector('.menu-mobile-container');
+const hamburgerBtn = document.querySelector('#hamburger-button');
+const menuBtn = document.querySelectorAll('.menu-button');
+const toggleMenu = (e) => {
+  e.stopPropagation();
+  mobileContainer.classList.toggle('sm-none');
+};
+closeBtn.addEventListener('click', toggleMenu);
+hamburgerBtn.addEventListener('click', toggleMenu);
+menuBtn.forEach((item) => {
+  item.addEventListener('click', toggleMenu);
 });
-
-alert('Hello');
