2c43e06db73f3d77a804219eed73814b9e3c3eae
Martin Perez Bazan <martin.perezbazan@gmail.com>
2022-10-13 12:41:38 -0300

    Fix script.js - add new images

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const projects = [
   {
     name: 'Profesional Art Printing Data More',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    image: 'image1.png',
+    image: 'Snapshoot-Professional.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     liveLink: 'https://mperezbazan.github.io/',
     sourceLink: 'https://github.com/mperezbazan',
@@ -18,7 +18,7 @@ const projects = [
   {
     name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    image: 'Image-DataDashboard.png',
+    image: 'Snapshoot-Data.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     liveLink: 'https://mperezbazan.github.io/',
     sourceLink: 'https://github.com/mperezbazan',
@@ -26,7 +26,7 @@ const projects = [
   {
     name: 'Website Portfolio',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    image: 'Image-WebsitePortfolio.png',
+    image: 'Snapshoot-Website.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     liveLink: 'https://mperezbazan.github.io/',
     sourceLink: 'https://github.com/mperezbazan',
@@ -34,7 +34,7 @@ const projects = [
   {
     name: 'Profesional Art Printing Data More',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    image: 'image1.png',
+    image: 'Snapshoot-Professional.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     liveLink: 'https://mperezbazan.github.io/',
     sourceLink: 'https://github.com/mperezbazan',
@@ -42,7 +42,7 @@ const projects = [
   {
     name: 'Data Dashboard Healthcare',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    image: 'Image-DataDashboard.png',
+    image: 'Snapshoot-Data.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     liveLink: 'https://mperezbazan.github.io/',
     sourceLink: 'https://github.com/mperezbazan',
@@ -50,7 +50,7 @@ const projects = [
   {
     name: 'Website Portfolio',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
-    image: 'Image-WebsitePortfolio.png',
+    image: 'Snapshoot-Website.png',
     technologies: ['html', 'bootstrap', 'Ruby'],
     liveLink: 'https://mperezbazan.github.io/',
     sourceLink: 'https://github.com/mperezbazan',
@@ -61,6 +61,7 @@ const closeBtn = document.querySelector('.close-button');
 const mobileContainer = document.querySelector('.menu-mobile-container');
 const hamburgerBtn = document.querySelector('#hamburger-button');
 const menuBtn = document.querySelectorAll('.menu-button');
+const closeModalButton = document.querySelector('.modal-button-close');
 const modal = document.querySelector('#project-modal');
 function toggleMenu() {
   mobileContainer.classList.toggle('display-none');
@@ -70,13 +71,14 @@ hamburgerBtn.addEventListener('click', toggleMenu);
 menuBtn.forEach((item) => {
   item.addEventListener('click', toggleMenu);
 });
-
 closeModalButton.addEventListener('click', () => {
   modal.classList.add('display-none');
 });
+
 const container = document.getElementById('Portfolio');
 projects.forEach((project, index) => {
   const article = document.createElement('article');
+  article.id = `project-${index}`;
   let test = '';
   if (index === 0) {
     article.classList.add('work-container', 'work-main');
@@ -98,7 +100,7 @@ projects.forEach((project, index) => {
     test += `
         </ul>
         <div class="work-footer-container-light">
-          <a class="button">
+          <a class="button project-button" id='btn-${index}'>
             See Project
           </a>
         </div>
@@ -111,6 +113,9 @@ projects.forEach((project, index) => {
     }
     if (index === 3 || index === 6) {
       article.classList.add('website-image');
+    }
+    if (index === 1 || index === 4) {
+      article.classList.add('professional-image');
     }
     test += `
       <div class="work-content-dark">
@@ -128,7 +133,7 @@ projects.forEach((project, index) => {
         </ul>
       </div>
       <div class="work-footer-container-dark lg-none">
-        <a class="work-button button dark fullwidth">
+        <a class="project-button work-button button dark fullwidth" id="btn-${index}">
           See Project
         </a>
       </div>
@@ -137,7 +142,6 @@ projects.forEach((project, index) => {
   article.innerHTML = test;
   container.appendChild(article);
 });
-
 const showModal = (id) => {
   const index = id.substring(id.indexOf('-') + 1, id.length);
   modal.classList.remove('display-none');
@@ -157,31 +161,9 @@ const showModal = (id) => {
   });
   modalImage.src = `./images/${projects[index].image}`;
   modalLiveButton.href = projects[index].liveLink;
-
-  const showModal = (id) => {
-    const index = id.substring(id.indexOf('-') + 1, id.length);
-    modal.classList.remove('display-none');
-    const modalTitle = document.querySelector('.modal-title');
-    const modalImage = document.querySelector('.modal-image');
-    const modalText = document.querySelector('.modal-text');
-    const modalLiveButton = document.getElementById('liveButton');
-    const modalSourceButton = document.getElementById('sourceButton');
-    const modalTechnologies = document.querySelector('.modal-tags');
-    modalTitle.textContent = projects[index].name;
-    modalText.textContent = projects[index].description;
-    modalTechnologies.innerHTML = '';
-    projects[index].technologies.forEach((technology) => {
-      const liTechnologies = document.createElement('li');
-      liTechnologies.innerHTML = `<li class="work-tag light modal-tag">${technology}</li>`;
-
-      modalTechnologies.appendChild(liTechnologies);
-    });
-    modalImage.src = `./images/${projects[index].image}`;
-    modalLiveButton.href = projects[index].liveLink;
-    modalSourceButton.href = projects[index].sourceLink;
-  };
-
-  const cards = document.querySelectorAll('.bg-image');
+  modalSourceButton.href = projects[index].sourceLink;
+};
+const cards = document.querySelectorAll('.bg-image');
 cards.forEach((card) => {
   card.addEventListener('mouseenter', (e) => {
     e.target.classList.toggle('pointer');
@@ -202,15 +184,14 @@ cards.forEach((card) => {
     showModal(id);
   });
 });
-
-  const buttons = document.querySelectorAll('.project-button');
-  buttons.forEach((button) => {
-    button.addEventListener('touchend', (e) => {
-      const { id } = e.target;
-      showModal(id);
-    });
-    button.addEventListener('click', (e) => {
-      const { id } = e.target;
-      showModal(id);
-    });
+const buttons = document.querySelectorAll('.project-button');
+buttons.forEach((button) => {
+  button.addEventListener('touchend', (e) => {
+    const { id } = e.target;
+    showModal(id);
   });
+  button.addEventListener('click', (e) => {
+    const { id } = e.target;
+    showModal(id);
+  });
+});
